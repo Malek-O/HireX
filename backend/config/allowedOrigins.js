0e9509d2f8aff37a8bb09c0e19608f055dd8a302
Malek-O <lslsls96996@gmail.com>
2023-11-10 21:21:36 +0300

    chore: init Folders &  server init

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const whitelist = [
+    'http://127.0.0.1:3550',
+    'http://127.0.0.1:5173',
+];
+
+module.exports = whitelist
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3550
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+const credentials = require('./middlewares/credentials');
+
+app.use(credentials)
+app.use(cors(require('./config/corsOptions')));
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false, limit: '5mb', parameterLimit: 50000 }));
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Welcome to HireX API" })
+})
+
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
